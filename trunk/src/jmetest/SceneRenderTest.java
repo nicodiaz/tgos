39176e6e713f8fd00ca3e8f77e11a4a20fe30c69
unidev.ar <unidev.ar@05ce8baa-f368-11de-bb94-7564724c0657>
2009-12-28 15:05:14 +0000

    Caso de Testeo: Modificaciones y mejoras al testing de jme Falla pendiente:

--- trunk/src/jmetest/SceneRenderTest.java
+++ trunk/src/jmetest/SceneRenderTest.java
@@ -9,7 +9,10 @@ import com.jme.image.Texture;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.scene.shape.Arrow;
+import com.jme.scene.shape.AxisRods;
+import com.jme.scene.shape.GeoSphere;
 import com.jme.scene.shape.Sphere;
+import com.jme.scene.shape.Teapot;
 import com.jme.scene.state.BlendState;
 import com.jme.scene.state.TextureState;
 import com.jme.util.TextureManager;
@@ -17,84 +20,118 @@ import com.jme.util.resource.MultiFormatResourceLocator;
 import com.jme.util.resource.ResourceLocatorTool;
 
 /**
- * <code>TestSphere</code>
- * @author Mark Powell
- * @version $Id: TestSphere.java 4130 2009-03-19 20:04:51Z blaine.dev $
+ * <code>SceneRenderTest</code>
+ * 
+ * This class is to test the principal shapes of the framework JMonkey
+ * 
+ * @author Hari
  */
-public class SceneRenderTest extends SimpleGame {
+public class SceneRenderTest extends SimpleGame
+{
 
-  private Quaternion rotQuat = new Quaternion();
-  private float angle = 0;
-  private Vector3f axis = new Vector3f(1, 1, 0);
-  private Sphere s;
-  private Arrow a;
+	private Quaternion rotQuat = new Quaternion();
+	private float angle = 0;
+	private Vector3f axis = new Vector3f(1, 1, 0);
+	private Sphere s;
+	private Arrow a;
 
-  /**
-   * Entry point for the test,
-   * @param args
-   */
-  public static void main(String[] args) {
-	SceneRenderTest app = new SceneRenderTest();
-    app.setConfigShowMode(ConfigShowMode.AlwaysShow);
-    app.start();
-  }
+	/**
+	 * Entry point for the test,
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		SceneRenderTest app = new SceneRenderTest();
+		app.setConfigShowMode(ConfigShowMode.AlwaysShow);
+		app.start();
+	}
 
-  protected void simpleUpdate() {
-    if (tpf < 1) {
-      angle = angle + (tpf * 1);
-      if (angle > 360) {
-        angle = 0;
-      }
-    }
-    rotQuat.fromAngleAxis(angle, axis);
-    s.setLocalRotation(rotQuat);
-  }
+	protected void simpleUpdate()
+	{
+		if (tpf < 1)
+		{
+			angle = angle + (tpf * 1);
+			if (angle > 360)
+			{
+				angle = 0;
+			}
+		}
+		rotQuat.fromAngleAxis(angle, axis);
+		s.setLocalRotation(rotQuat);
+	}
 
-  /**
-   * builds the trimesh.
-   * @see com.jme.app.SimpleGame#initGame()
-   */
-  protected void simpleInitGame() {
-    display.setTitle("jME - Sphere");
+	/**
+	 * builds the trimesh.
+	 * 
+	 * @see com.jme.app.SimpleGame#initGame()
+	 */
+	protected void simpleInitGame()
+	{
+		display.setTitle("jME - Sphere");
 
-    s = new Sphere("Sphere", 63, 50, 25);
-    s.setLocalTranslation(new Vector3f(0,0,-40));
-    s.setModelBound(new BoundingBox());
-    s.updateModelBound();
-    rootNode.attachChild(s);
+		// Sphere figure Test
+		s = new Sphere("Sphere", 63, 50, 25.0F);
+		s.setLocalTranslation(new Vector3f(0.0F, 0.0F, -350.0F));
+		s.setModelBound(new BoundingBox());
+		s.updateModelBound();
+		rootNode.attachChild(s);
 
-    a = new Arrow("Arrow", 10.0F, 5.0F);
-    a.setLocalTranslation(new Vector3f(70, 70, -40));
-    a.setModelBound(new BoundingBox());
-    a.updateModelBound();
-    rootNode.attachChild(a);
-    
-    try {
-        MultiFormatResourceLocator loc2 = new MultiFormatResourceLocator(new File("c:/").toURI(), ".jpg", ".png", ".tga");
-        ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, loc2);
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    
-    URL u = ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_TEXTURE, "/model/grass.gif");
-    System.err.println("FOUND URL: "+u);
-    
-    TextureState ts = display.getRenderer().createTextureState();
-    ts.setEnabled(true);
-    ts.setTexture(
-        TextureManager.loadTexture(u,
-        Texture.MinificationFilter.Trilinear,
-        Texture.MagnificationFilter.Bilinear));
+		// Arrorw Figure test
+		a = new Arrow("Arrow", 10.0F, 5.0F);
+		a.setLocalTranslation(new Vector3f(10.0F, 10.0F, -40.0F));
+		a.setModelBound(new BoundingBox());
+		a.updateModelBound();
+		rootNode.attachChild(a);
 
-    rootNode.setRenderState(ts);
-    
-    BlendState alpha = display.getRenderer().createBlendState();
-    alpha.setBlendEnabled(true);
-    alpha.setSourceFunction(BlendState.SourceFunction.SourceAlpha);
-    alpha.setDestinationFunction(BlendState.DestinationFunction.OneMinusSourceAlpha);
-    alpha.setTestEnabled(true);
-    alpha.setTestFunction(BlendState.TestFunction.GreaterThan);
-    alpha.setEnabled(true);
-    rootNode.setRenderState(alpha);
-  }
+		// Teapot figure test
+		Teapot t = new Teapot("teapot");
+		t.setLocalTranslation(new Vector3f(-10.0F, -10.0F, -40.0F));
+		t.setModelBound(new BoundingBox());
+		t.updateModelBound();
+		rootNode.attachChild(t);
+
+		// GeoSphere figure test
+		GeoSphere g = new GeoSphere("GeoSphere", true, 1);
+		g.setLocalTranslation(new Vector3f(0F, -10.0F, -40.0F));
+		g.setModelBound(new BoundingBox());
+		g.updateModelBound();
+		rootNode.attachChild(g);
+
+		// Create an right handed axisrods object with a scale of 1/2
+		AxisRods ar = new AxisRods("rods", true, 0.5f);
+		// Attach ar to the node we want to visualize
+		rootNode.attachChild(ar);
+
+		try
+		{
+			MultiFormatResourceLocator loc2 = new MultiFormatResourceLocator(new File("c:/")
+				.toURI(), ".jpg", ".png", ".tga");
+			ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, loc2);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		URL u = ResourceLocatorTool.locateResource(ResourceLocatorTool.TYPE_TEXTURE,
+			"/model/grass.gif");
+		System.err.println("FOUND URL: " + u);
+
+		TextureState ts = display.getRenderer().createTextureState();
+		ts.setEnabled(true);
+		ts.setTexture(TextureManager.loadTexture(u, Texture.MinificationFilter.Trilinear,
+			Texture.MagnificationFilter.Bilinear));
+
+		rootNode.setRenderState(ts);
+
+		BlendState alpha = display.getRenderer().createBlendState();
+		alpha.setBlendEnabled(true);
+		alpha.setSourceFunction(BlendState.SourceFunction.SourceAlpha);
+		alpha.setDestinationFunction(BlendState.DestinationFunction.OneMinusSourceAlpha);
+		alpha.setTestEnabled(true);
+		alpha.setTestFunction(BlendState.TestFunction.GreaterThan);
+		alpha.setEnabled(true);
+		rootNode.setRenderState(alpha);
+	}
 }
