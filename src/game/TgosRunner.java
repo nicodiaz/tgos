69b5b564984a3c735ffb71f423bea4b75abfccf7
unidev.ar <unidev.ar@05ce8baa-f368-11de-bb94-7564724c0657>
2010-01-30 00:44:01 +0000

    Caso de Uso: Habitacion inicial y configuracion iniciales. Bug: Ultima Modificacion:

---
+++ src/game/TgosRunner.java
@@ -0,0 +1,37 @@
+package game;
+
+import game.main.TgosMain;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The Game Of Sapo ----------------
+ * 
+ * This is the main class of the game. From this class, the game is launched and start
+ * running.
+ * 
+ */
+public class TgosRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// to see the important stuff
+		Logger.getLogger("").setLevel(Level.WARNING); 
+		
+		// Make a instance of the game
+		TgosMain mainApp = new TgosMain();
+		
+		// Show the configurations
+//		mainApp.setConfigShowMode(ConfigShowMode.AlwaysShow);
+		
+		// Run the game
+		mainApp.start();
+
+	}
+
+}
