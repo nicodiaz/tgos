5e1251607f35c74f8621e1191a75ce238650cadd
unidev.ar <unidev.ar@05ce8baa-f368-11de-bb94-7564724c0657>
2010-01-30 00:44:01 +0000

    Caso de Uso: Habitacion inicial y configuracion iniciales. Bug: Ultima Modificacion:

---
+++ trunk/src/game/elements/RoomFactory.java
@@ -0,0 +1,72 @@
+package game.elements;
+
+import com.jme.math.Vector3f;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Box;
+import com.jmex.physics.PhysicsSpace;
+import com.jmex.physics.StaticPhysicsNode;
+
+
+/**
+ * This factory creates rooms for the game. However, the idea is to have only one room for
+ * the TGOS game.
+ * 
+ * @author Hari
+ *
+ */
+public class RoomFactory
+{
+	private StaticPhysicsNode room = null;
+	
+	// this factor is the size of bigger for lateral
+	private Float SIZEFACTOR = 20.0f;
+	
+	// doesn't care, how deeper is the floor/wall
+	private Float ESPESOR = 1.5f;
+	
+	// How far is the front wall
+	private Float FAR = 80.0f;
+	
+	
+	
+	/*
+	 * TODO: Make a parameter in the constructor to generate a random room.
+	 * 
+	 */
+	public RoomFactory(PhysicsSpace theSpace, Node rootNode)
+	{
+		room = theSpace.createStaticNode();
+		rootNode.attachChild(room);
+		
+//		//Now we use the boxes of JME, that are trimeshes. We start with the front Wall. 
+//		final Box frontWallBox = new Box("frontWall", new Vector3f(0, 0, -FAR), SIZEFACTOR, SIZEFACTOR, ESPESOR);
+//		room.attachChild(frontWallBox);
+//
+//		// The floor
+//		final Box floorBox = new Box("floor", new Vector3f(0, -SIZEFACTOR, 0), SIZEFACTOR, ESPESOR, FAR);
+//		room.attachChild(floorBox);
+//
+//		// The roof
+//		final Box roofBox = new Box("roof", new Vector3f(0, SIZEFACTOR, 0), SIZEFACTOR, ESPESOR, FAR);
+//		room.attachChild(roofBox);
+//		
+//		// The right wall
+//		final Box rightWallBox = new Box("rightWall", new Vector3f(SIZEFACTOR, 0, 0), ESPESOR, SIZEFACTOR, FAR);
+//		room.attachChild(rightWallBox);
+//		
+//		// The left wall
+//		final Box leftWallBox = new Box("leftWall", new Vector3f(-SIZEFACTOR, 0, 0), ESPESOR, SIZEFACTOR, FAR);
+//		room.attachChild(leftWallBox);
+		
+		// The room
+		final Box roomBox = new Box("room", new Vector3f(0, 0, 0), SIZEFACTOR, SIZEFACTOR, FAR);
+		room.attachChild(roomBox);
+		
+		room.generatePhysicsGeometry();
+	}
+	
+	public StaticPhysicsNode getRoom()
+	{
+		return room;
+	}
+}
---
+++ trunk/src/game/main/TgosMain.java
@@ -0,0 +1,19 @@
+package game.main;
+
+import game.elements.RoomFactory;
+
+import com.jmex.physics.util.SimplePhysicsGame;
+
+public class TgosMain extends SimplePhysicsGame
+{
+	
+	@Override
+	protected void simpleInitGame()
+	{
+		// We start creating the room
+		RoomFactory roomFactory = new RoomFactory(getPhysicsSpace(), rootNode);
+		
+	}
+	
+
+}
