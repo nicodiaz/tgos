9850aa32b978e5f57964ca9bf2bb67ed0c34dcee
unidev.ar <unidev.ar@05ce8baa-f368-11de-bb94-7564724c0657>
2010-01-31 21:51:04 +0000

    Caso de Uso: Creacion del cuarto de juego Bug: Ultima Modificacion: Ahora el contructor está parametrizado, para luego leer las cosas desde un XML

--- src/game/elements/RoomFactory.java
+++ src/game/elements/RoomFactory.java
@@ -22,43 +22,46 @@ public class RoomFactory
 	private Float SIZEFACTOR = 20.0f;
 	
 	// doesn't care, how deeper is the floor/wall
-	private Float ESPESOR = 1.5f;
+	private Float THICK = 1.5f;
 	
 	// How far is the front wall
 	private Float FAR = 80.0f;
 	
 	
-	
-	/*
-	 * TODO: Make a parameter in the constructor to generate a random room.
-	 * 
-	 */
 	public RoomFactory(PhysicsSpace theSpace, Node rootNode)
+	{
+		this(theSpace, rootNode, 20.0f, 1.5f, 80.0f);
+	}
+	
+	public RoomFactory(PhysicsSpace theSpace, Node rootNode, float size, float thick, float far)
 	{
 		room = theSpace.createStaticNode();
 		rootNode.attachChild(room);
 		
 //		//Now we use the boxes of JME, that are trimeshes. We start with the front Wall. 
-//		final Box frontWallBox = new Box("frontWall", new Vector3f(0, 0, -FAR), SIZEFACTOR, SIZEFACTOR, ESPESOR);
+//		final Box frontWallBox = new Box("frontWall", new Vector3f(0, 0, -FAR), SIZEFACTOR, SIZEFACTOR, THICK);
 //		room.attachChild(frontWallBox);
 //
 //		// The floor
-//		final Box floorBox = new Box("floor", new Vector3f(0, -SIZEFACTOR, 0), SIZEFACTOR, ESPESOR, FAR);
+//		final Box floorBox = new Box("floor", new Vector3f(0, -SIZEFACTOR, 0), SIZEFACTOR, THICK, FAR);
 //		room.attachChild(floorBox);
 //
 //		// The roof
-//		final Box roofBox = new Box("roof", new Vector3f(0, SIZEFACTOR, 0), SIZEFACTOR, ESPESOR, FAR);
+//		final Box roofBox = new Box("roof", new Vector3f(0, SIZEFACTOR, 0), SIZEFACTOR, THICK, FAR);
 //		room.attachChild(roofBox);
 //		
 //		// The right wall
-//		final Box rightWallBox = new Box("rightWall", new Vector3f(SIZEFACTOR, 0, 0), ESPESOR, SIZEFACTOR, FAR);
+//		final Box rightWallBox = new Box("rightWall", new Vector3f(SIZEFACTOR, 0, 0), THICK, SIZEFACTOR, FAR);
 //		room.attachChild(rightWallBox);
 //		
 //		// The left wall
-//		final Box leftWallBox = new Box("leftWall", new Vector3f(-SIZEFACTOR, 0, 0), ESPESOR, SIZEFACTOR, FAR);
+//		final Box leftWallBox = new Box("leftWall", new Vector3f(-SIZEFACTOR, 0, 0), THICK, SIZEFACTOR, FAR);
 //		room.attachChild(leftWallBox);
 		
-		// The room
+		this.SIZEFACTOR = size;
+		this.THICK = thick;
+		this.FAR = far;
+		
 		final Box roomBox = new Box("room", new Vector3f(0, 0, 0), SIZEFACTOR, SIZEFACTOR, FAR);
 		room.attachChild(roomBox);
 		
