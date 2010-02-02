82a4a2efa55da12c5a8efcc20c73f89f05aa3d0e
unidev.ar <unidev.ar@05ce8baa-f368-11de-bb94-7564724c0657>
2010-02-02 15:26:50 +0000

    Modulo: Modelado de la escena. Caja del Sapo. Modificacion: Modelado con primitivas de JME. Ya se encuentran hechas casi las canaletas internas

--- trunk/src/game/elements/Sapo.java
+++ trunk/src/game/elements/Sapo.java
@@ -1,7 +1,6 @@
 package game.elements;
 
 import com.jme.math.FastMath;
-import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import com.jme.scene.shape.Box;
@@ -33,6 +32,11 @@ public class Sapo
 	private Float innerFallsInclination = FastMath.PI / 4.0f;
 	private Float innerFallsThick = 0.1f;
 
+	// Boxes variables
+	private Float boxesLength = 2.0f;
+	private Float boxesThick = 0.05f;
+	private Float boxesHigh = 1.5f;
+
 	public Sapo(PhysicsSpace theSpace, Node rootNode)
 	{
 		space = theSpace;
@@ -43,6 +47,7 @@ public class Sapo
 
 		makeWalls();
 		makeInnerFalls();
+		makeBoxes();
 
 		// And finally, generate the require physics
 		sapoStaticNode.generatePhysicsGeometry();
@@ -55,6 +60,9 @@ public class Sapo
 
 	}
 
+	/*
+	 * Method to make the floor, the back door, and side walls.
+	 */
 	private void makeWalls()
 	{
 		// Now we use the boxes of JME, that are trimeshes.
@@ -69,21 +77,23 @@ public class Sapo
 			-(sapoHeight + sapoThick), -sapoZMid), sapoWidth, sapoThick,
 			(sapoBack - sapoFront) / 2.0f);
 		sapoStaticNode.attachChild(floorSapoWall);
-		
-		// TODO:Only for DEBUG. Modify later
+
+		// TODO:Only for DEBUG. Modify later -> makeBoxes();
 		// front wall.
-//		final Box frontSapoWall = new Box("frontSapoWall", new Vector3f(0, 0,
-//			-sapoFront), sapoWidth, sapoHeight, sapoThick / 10);
-//		sapoStaticNode.attachChild(frontSapoWall);
+		// final Box frontSapoWall = new Box("frontSapoWall", new Vector3f(0, 0,
+		// -sapoFront), sapoWidth, sapoHeight, sapoThick / 10);
+		// sapoStaticNode.attachChild(frontSapoWall);
 	}
 
-	// TODO: Modify the "heigts" of the falls.
+	/*
+	 * Method to create the inner falls, all of them
+	 */
 	private void makeInnerFalls()
 	{
 
 		// We start with the lower fall.
-		final Box lowerFall = new Box("lowerFall", new Vector3f(0, 0, 0), sapoWidth, sapoHeight * 1.4f,
-			innerFallsThick);
+		final Box lowerFall = new Box("lowerFall", new Vector3f(0, 0, 0), sapoWidth,
+			sapoHeight * 1.4f, innerFallsThick);
 		lowerFall.getLocalRotation().fromAngleNormalAxis(innerFallsInclination,
 			new Vector3f(-1, 0, 0));
 		lowerFall.getLocalTranslation().set(new Vector3f(0, 0, -sapoZMid));
@@ -108,8 +118,50 @@ public class Sapo
 		upperFall.getLocalRotation().fromAngleNormalAxis(innerFallsInclination,
 			new Vector3f(-1, 0, 0));
 		incY = (sapoZMid - sapoFront) / 2.0f;
-		upperFall.getLocalTranslation()
-		.set(new Vector3f(0, sapoHeight / 2.0f, -(sapoZMid - incY)));
+		upperFall.getLocalTranslation().set(new Vector3f(0, sapoHeight / 2.0f, -(sapoZMid - incY)));
 		sapoStaticNode.attachChild(upperFall);
+	}
+
+	/*
+	 * Method to create the front boxes, where the coin will fall inside.
+	 */
+	private void makeBoxes()
+	{
+		Vector3f lowerPoint = new Vector3f(0, -(sapoHeight - boxesThick),
+			-(sapoFront - boxesLength));
+		/*
+		 * We start with the lower boxes. "Boxes" is a way to say "deposit", because the
+		 * idea is make somewhere where the coin will fall. The inner walls will divide
+		 * this into boxes.
+		 */
+		final Box lowerBoxesFloor = new Box("lowerBoxesFloor", lowerPoint, sapoWidth, boxesThick,
+			boxesLength);
+		sapoStaticNode.attachChild(lowerBoxesFloor);
+		final Box lowerBoxesFront = new Box("lowerBoxesFront", lowerPoint.add(0, boxesHigh, boxesLength), sapoWidth, boxesHigh,
+			boxesThick);
+		sapoStaticNode.attachChild(lowerBoxesFront);
+		
+		/*
+		 * Ok. Now, with the others boxes, we have to do the same, changing only the center point.
+		 */
+		Vector3f middlePoint = lowerPoint.add(0, (sapoZMid - sapoFront) / 2.0f, 0);
+		final Box middleBoxesFloor = new Box("middleBoxesFloor", middlePoint, sapoWidth, boxesThick,
+			boxesLength);
+		sapoStaticNode.attachChild(middleBoxesFloor);
+		final Box middleBoxesFront = new Box("middleBoxesFront", middlePoint.add(0, boxesHigh, boxesLength), sapoWidth, boxesHigh,
+			boxesThick);
+		sapoStaticNode.attachChild(middleBoxesFront);
+		
+		/*
+		 * Finally, the last one
+		 */
+		Vector3f upperPoint = lowerPoint.add(0, (sapoZMid - sapoFront), 0);
+		final Box upperBoxesFloor = new Box("upperBoxesFloor", upperPoint, sapoWidth, boxesThick,
+			boxesLength);
+		sapoStaticNode.attachChild(upperBoxesFloor);
+		final Box upperBoxesFront = new Box("upperBoxesFront", upperPoint.add(0, boxesHigh, boxesLength), sapoWidth, boxesHigh,
+			boxesThick);
+		sapoStaticNode.attachChild(upperBoxesFront);
+
 	}
 }
